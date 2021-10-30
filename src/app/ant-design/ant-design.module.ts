a5491d6a3373f9bb72dc27c03d29e59ab38cc7bf
prameshbajra <pe.messh@gmail.com>
2021-10-30 11:28:06 +0545

    [WIP] Results component in play.

--- src/app/ant-design/ant-design.module.ts
+++ src/app/ant-design/ant-design.module.ts
@@ -7,6 +7,8 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzTableModule } from 'ng-zorro-antd/table';
+
 
 @NgModule({
     declarations: [],
@@ -18,6 +20,7 @@ import { NzSpinModule } from 'ng-zorro-antd/spin';
         NzInputModule,
         NzLayoutModule,
         NzSelectModule,
+        NzTableModule,
         NzSpinModule
     ],
     exports: [
@@ -27,6 +30,7 @@ import { NzSpinModule } from 'ng-zorro-antd/spin';
         NzInputModule,
         NzLayoutModule,
         NzSelectModule,
+        NzTableModule,
         NzSpinModule
     ]
 })
--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResponseCountry } from 'src/app/models/responseCountry';
 
 @Component({
     selector: 'app-results',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
-    loading: boolean = false;
+    public loading: boolean = false;
+    public countryResults: ResponseCountry[];
 
-    constructor() { }
+    constructor() {
+        this.countryResults = [
+            {
+                "countryName": "Nepal",
+                "testStatus": "Failed"
+            },
+            {
+                "countryName": "India",
+                "testStatus": "Failed"
+            },
+            {
+                "countryName": "Germany",
+                "testStatus": "Successful"
+            }
+        ];
+    }
 
     ngOnInit(): void {
     }
