ccd35c95fd9eb6c608b5dea73a599afc1a633b6c
prameshbajra <pe.messh@gmail.com>
2021-10-30 15:57:44 +0545

    [WIP] Mobile friendly starts here ...

--- src/app/ant-design/ant-design.module.ts
+++ src/app/ant-design/ant-design.module.ts
@@ -1,12 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzMessageModule } from 'ng-zorro-antd/message';
 
@@ -17,24 +12,14 @@ import { NzMessageModule } from 'ng-zorro-antd/message';
     imports: [
         CommonModule,
         NzButtonModule,
-        NzGridModule,
-        NzIconModule,
-        NzInputModule,
-        NzLayoutModule,
         NzSelectModule,
         NzTableModule,
-        NzSpinModule,
         NzMessageModule
     ],
     exports: [
         NzButtonModule,
-        NzGridModule,
-        NzIconModule,
-        NzInputModule,
-        NzLayoutModule,
         NzSelectModule,
         NzTableModule,
-        NzSpinModule,
         NzMessageModule
     ]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,8 +39,6 @@ registerLocaleData(en);
         ReactiveFormsModule,
         ServiceWorkerModule.register('ngsw-worker.js', {
             enabled: environment.production,
-            // Register the ServiceWorker as soon as the app is stable
-            // or after 30 seconds (whichever comes first).
             registrationStrategy: 'registerWhenStable:30000'
         })
     ],
